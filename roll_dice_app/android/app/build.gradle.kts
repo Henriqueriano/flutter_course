f2fd29e6e28b1746c18e375187e5516829b12488
Daniel Henrique <daniel.neves0021@gmail.com>
2025-11-23 13:35:59 -0300

    feat: build apk

--- roll_dice_app/android/app/build.gradle.kts
+++ roll_dice_app/android/app/build.gradle.kts
@@ -28,6 +28,11 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        ndk {
+            // Filter for architectures supported by Flutter
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+        }
     }
 
     buildTypes {
